d0b2b41b431f2d541f51fa0a4e6f51327ea6fc6b
sivan <sivsivlevy@gmail.com>
2019-09-16 16:06:04 -0700

    sdlfk

--- src/Actors.h
+++ src/Actors.h
@@ -2,13 +2,14 @@
 #define ACTORS_INCLUDED
 
 #include "Position.h"
-// actors class
-
 
 class Actors
 {
 public:
 	Actors(int x, int y);
+	virtual void moveActor(char mover)=0;  // this one sets the position based on what the user input
+	virtual void spawn() = 0; // randomly places actors
+
 
 private:
 	Position mypos;
