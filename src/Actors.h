8b00140140a7e85a87e242d36be5868645f1f1b6
sivan <sivsivlevy@gmail.com>
2019-09-16 15:19:23 -0700

    hello

--- src/Actors.h
+++ src/Actors.h
@@ -3,19 +3,19 @@
 
 // actors class
 
+
 class Actors
 {
 public:
-	Actors(int x, int y);  //
-	//function to move // players will move with whatever input
-	//destroyed function
+	
 
 private:
-	//position
-	int m_x;
-	int m_y;
+	
+	
 };
 
 #endif //ACTORS_INCLUDED
 
 //hey
+
+//hello
