c07371899af18905f080d6a6fca278f22ba0f794
Dan <jakq277@berkeley.edu>
2019-09-16 23:47:20 -0700

    the wokring one

--- src/Bomb.cpp
+++ src/Bomb.cpp
@@ -9,24 +9,12 @@ int Bomb::getDamage() const{
 int Bomb::getTime() const{
     return time;
 }
-<<<<<<< HEAD
 void Bomb::setRange(int x){
     range = x;
-    }it
+    }
 void Bomb::setDamage(int x){
         damage = x;
     }
-void Bomb::setTime(int){
+void Bomb::setTime(int x){
         time = x;
     }
-=======
-// void Bomb::setRange(int x){
-//     range = x;
-//     }
-// void Bomb::setDamage(int x){
-//         damage = x;
-//     }
-// void Bomb::setTime(int){
-//         time = x;
-//     }
->>>>>>> 7c9c45c2cf8362458766bce377c3e759f0d349e8
