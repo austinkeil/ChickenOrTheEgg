d26871f856cb3744b8dc574754f2facb75db129f
Austin Keil <austin.c.keil@gmail.com>
2019-09-16 23:05:44 -0700

    test

--- src/Bomb.cpp
+++ src/Bomb.cpp
@@ -9,24 +9,12 @@ int Bomb::getDamage() const{
 int Bomb::getTime() const{
     return time;
 }
-<<<<<<< HEAD
-
-void Bomb::setRange(int x){
-    range = x;
-    }
-
-void Bomb::setDamage(int x){
-        damage = x;
-    }
-
-=======
 void Bomb::setRange(int x){
     range = x;
     }
 void Bomb::setDamage(int x){
         damage = x;
     }
->>>>>>> 928f0fb645d4d197c4b5bc75d5b5d2bc0d8e63f0
 void Bomb::setTime(int x){
         time = x;
     }
--- src/Bomb.h
+++ src/Bomb.h
@@ -13,12 +13,6 @@ class Bomb: public GameObject{
     void setRange(int x);//setters
     void setDamage(int x);
     void setTime(int x);
-<<<<<<< HEAD
-=======
-    // void setRange();//setters
-    // void setDamage();
-    // void setTime();
->>>>>>> 928f0fb645d4d197c4b5bc75d5b5d2bc0d8e63f0
 
 };
 
